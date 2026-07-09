c6d07fa4eecedc9d3c9c4249ff3e4c74b76cc028
sravani192019 <sravani.jamithireddy@gmail.com>
2026-07-09 10:00:57 +0530

    EXPOSE

---
+++ terraform-Devops90s/14-import/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.50.0"
+    }
+  }
+ 
+  backend "s3" {
+  
+  
+  }
+}
+
+
+provider "aws" {
+  # Configuration options
+}
